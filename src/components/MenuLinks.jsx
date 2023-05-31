3572c6c8f6e2794e7d5ca92ad8c7647bd051ae25
clerov <byteblazedev@gmail.com>
2023-05-31 21:32:10 +0530

    updated

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -1,37 +1,59 @@
-import PropTypes from 'prop-types'
-const MenuLinks = ({setStatus}) => {
-    return ( 
-        <>
-           <a href="#home" className="hover:text-rose-500 text-rose-500" onClick={(e)=>{
-              document.body.style.overflowY = "unset"
-              setStatus(false)
-           }}>
-                Home
-              </a>
-              <a href="#shop" className="hover:text-rose-500" onClick={(e)=>{
-                  e.preventDefault()
-                  document.body.style.overflowY = "unset"
-                  setStatus(false)
-                  location.href = "#shop"
-              }}>
-                Cakes
-              </a>
-              <a href="#testi" className="hover:text-rose-500" onClick={(e)=>{
-                  document.body.style.overflowY = "unset"
-                  setStatus(false)
-              }}>
-                Testimonial
-              </a>
-              <a href="#footer" className="hover:text-rose-500" onClick={(e)=>{
-                  document.body.style.overflowY = "unset"
-                  setStatus(false)
-              }}>
-                Contact
-              </a>
-        </>
-     );
-}
+import PropTypes from "prop-types";
+const MenuLinks = ({ setStatus }) => {
+  return (
+    <>
+      <a
+        href="#home"
+        className="hover:text-rose-500 text-rose-500"
+        onClick={(e) => {
+          e.preventDefault();
+          document.body.style.overflowY = "unset";
+          setStatus(false);
+          document.querySelector(e.currentTarget.href).scrollIntoView();
+        }}
+      >
+        Home
+      </a>
+      <a
+        href="#shop"
+        className="hover:text-rose-500"
+        onClick={(e) => {
+          e.preventDefault();
+          document.body.style.overflowY = "unset";
+          setStatus(false);
+          document.querySelector(e.currentTarget.href).scrollIntoView();
+        }}
+      >
+        Cakes
+      </a>
+      <a
+        href="#testi"
+        className="hover:text-rose-500"
+        onClick={(e) => {
+          e.preventDefault();
+          document.body.style.overflowY = "unset";
+          setStatus(false);
+          document.querySelector(e.currentTarget.href).scrollIntoView();
+        }}
+      >
+        Testimonial
+      </a>
+      <a
+        href="#footer"
+        className="hover:text-rose-500"
+        onClick={(e) => {
+          e.preventDefault();
+          document.body.style.overflowY = "unset";
+          setStatus(false);
+          document.querySelector(e.currentTarget.href).scrollIntoView();
+        }}
+      >
+        Contact
+      </a>
+    </>
+  );
+};
 MenuLinks.propTypes = {
-  setStatus:PropTypes.func
-}
+  setStatus: PropTypes.func,
+};
 export default MenuLinks;
