b8cfc1310765eabe208dd939ac80e70d1328508e
clerov <byteblazedev@gmail.com>
2023-05-31 21:22:28 +0530

    updated

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -1,20 +1,39 @@
-const MenuLinks = () => {
+import PropTypes from 'prop-types'
+const MenuLinks = ({setStatus}) => {
     return ( 
         <>
-           <a href="#home" className="hover:text-rose-500 text-rose-500">
+           <a href="#home" className="hover:text-rose-500 text-rose-500" onClick={(e)=>{
+              e.preventDefault()
+              document.body.style.overflowY = "unset"
+              setStatus(false)
+           }}>
                 Home
               </a>
-              <a href="#shop" className="hover:text-rose-500">
+              <a href="#shop" className="hover:text-rose-500" onClick={(e)=>{
+                 e.preventDefault()
+                  document.body.style.overflowY = "unset"
+                  setStatus(false)
+              }}>
                 Cakes
               </a>
-              <a href="#testi" className="hover:text-rose-500">
+              <a href="#testi" className="hover:text-rose-500" onClick={(e)=>{
+                 e.preventDefault()
+                  document.body.style.overflowY = "unset"
+                  setStatus(false)
+              }}>
                 Testimonial
               </a>
-              <a href="#footer" className="hover:text-rose-500">
+              <a href="#footer" className="hover:text-rose-500" onClick={(e)=>{
+                 e.preventDefault()
+                  document.body.style.overflowY = "unset"
+                  setStatus(false)
+              }}>
                 Contact
               </a>
         </>
      );
 }
- 
+MenuLinks.propTypes = {
+  setStatus:PropTypes.func
+}
 export default MenuLinks;
