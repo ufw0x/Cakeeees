1cfb8a54246adafabcb483536dc9e4208f85a375
clerov <byteblazedev@gmail.com>
2023-05-31 21:23:42 +0530

    updated

--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -3,28 +3,24 @@ const MenuLinks = ({setStatus}) => {
     return ( 
         <>
            <a href="#home" className="hover:text-rose-500 text-rose-500" onClick={(e)=>{
-              e.preventDefault()
               document.body.style.overflowY = "unset"
               setStatus(false)
            }}>
                 Home
               </a>
               <a href="#shop" className="hover:text-rose-500" onClick={(e)=>{
-                 e.preventDefault()
                   document.body.style.overflowY = "unset"
                   setStatus(false)
               }}>
                 Cakes
               </a>
               <a href="#testi" className="hover:text-rose-500" onClick={(e)=>{
-                 e.preventDefault()
                   document.body.style.overflowY = "unset"
                   setStatus(false)
               }}>
                 Testimonial
               </a>
               <a href="#footer" className="hover:text-rose-500" onClick={(e)=>{
-                 e.preventDefault()
                   document.body.style.overflowY = "unset"
                   setStatus(false)
               }}>
