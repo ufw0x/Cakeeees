ef44acaaca59ade495871794425fc0df43755f3b
clerov <byteblazedev@gmail.com>
2023-06-04 22:53:46 +0530

    cart features implemented

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,106 +1,148 @@
+import { useState, useEffect } from "react";
 import { MdClose } from "react-icons/md";
 import PropTypes from "prop-types";
 import { cakes } from "../constants/constants";
 import { toast } from "react-toastify";
 
 const Cart = ({ showCart }) => {
-  const getData = () => {
-    let arr = [];
-    for (let i = 0; i <= 9; i++) {
-      let x = localStorage.getItem(i);
-      if (x) {
-        arr.push({ ...cakes[i], quantity: x });
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      let arr = [];
+      for (let i = 0; i <= cakes.length; i++) {
+        let x = localStorage.getItem(i);
+        if (x) {
+          arr.push({ ...cakes[i], quantity: parseInt(x) });
+        }
       }
-    }
-    return arr;
+      setCartItems(arr);
+    };
+
+    getData();
+  }, []);
+
+  const updateQuantity = (itemId, quantity) => {
+    const updatedItems = cartItems.map((item) => {
+      if (item.id === itemId) {
+        localStorage.setItem(item.id, quantity);
+        return { ...item, quantity };
+      }
+      return item;
+    });
+
+    setCartItems(updatedItems);
   };
-  const cart = getData();
+
+  const clearCart = () => {
+    localStorage.clear();
+    toast.success("Your shopping cart cleared successfully", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+    setCartItems([]);
+    showCart(false);
+    document.body.style.overflowY = "unset";
+  };
+
+  const submitOrder = () => {
+    localStorage.clear();
+    toast.success("Your order is successful.", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+    setCartItems([]);
+    showCart(false);
+    document.body.style.overflowY = "unset";
+  };
+
+  const calculateTotalPrice = () => {
+    return cartItems.reduce((total, item) => {
+      return total + item.price * item.quantity;
+    }, 0);
+  };
   return (
-    <>
-      <aside className="overflow-y-scroll w-full flex flex-col h-full min-h-screen max-h-screen bg-[#ffffff6d] backdrop-blur-2xl fixed top-0 left-0 z-50">
+    <aside className="overflow-y-scroll w-full h-full fixed top-0 left-0 z-50 bg-gray-900 bg-opacity-50">
+      <div className="relative max-w-sm mx-auto mt-20 p-4 bg-white rounded-lg shadow-lg px-8 lg:px-10 py-10 lg:py-14 text-center">
         <MdClose
-          className="m-10 text-3xl absolute right-2 top-2"
-          role="button"
+          className="absolute top-2 right-2 text-gray-600 cursor-pointer"
+          size={24}
           onClick={() => {
             showCart(false);
-            document.body.style.overflowY = "unset"
+            document.body.style.overflowY = "unset";
           }}
         />
-        {cart.length > 0 ? (
-          <div className="px-10 pt-24 text-center flex flex-col justify-center items-center">
-            <h3 className="text-2xl font-bold text-rose-500 my-10">
-              Your Shopping Cart
-            </h3>
-            <table className="flex flex-col py-10 justify-center space-y-8 mx-auto text-center items-center">
-              <tr className="flex flex-row space-x-16 text-center justify-between">
-                <th>Image</th>
-                <th>Price</th>
-                <th>Quantity</th>
-              </tr>
-              {cart.map((c) => (
-                <>
-                  <tr
-                    key={c.id}
-                    className="flex flex-row justify-between space-x-20"
-                  >
+        <h3 className="text-2xl font-bold text-gray-800 mb-6">
+          Your Shopping Cart
+        </h3>
+        {cartItems.length > 0 ? (
+          <>
+            <table className="w-full mx-auto my-8 text-center">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id}>
                     <td>
                       <img
-                        src={c.image}
-                        alt={c.name}
-                        className="w-14 rounded-full"
+                        src={item.image}
+                        alt={item.name}
+                        className="w-12 h-12 rounded-full mx-auto"
                       />
                     </td>
                     <td>
-                      <h3 className="text-xl">{c.price}$</h3>
+                      <span className="text-gray-800">${item.price}</span>
                     </td>
                     <td>
-                      <h4 className="text-xl">{c.quantity}</h4>
+                      <input
+                        type="number"
+                        min="1"
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateQuantity(item.id, parseInt(e.target.value))
+                        }
+                        className="w-12 text-gray-800"
+                      />
                     </td>
                   </tr>
-                </>
-              ))}
+                ))}
+              </tbody>
             </table>
-            <div className="flex flex-row justify-between px-4 w-full py-10 flex-wrap">
-            <button
-              className="w-20 h-10  border-2 border-rose-500 text-rose-500 rounded-md "
-              onClick={() => {
-                localStorage.clear();
-                toast.success("Your shopping cart cleared successfully", {
-                  position: toast.POSITION.TOP_RIGHT,
-                });
-                showCart(false)
-                document.body.style.overflowY = "unset"
-              }}
-            >
-              Clear
-            </button>
-            <button
-              className="w-20 h-10  border-2 border-rose-500 text-rose-500 rounded-md"
-              onClick={() => {
-                localStorage.clear();
-                toast.success("Your order is successfull.", {
-                  position: toast.POSITION.TOP_RIGHT,
-                });
-                showCart(false)
-                document.body.style.overflowY = "unset"
-              }}
-            >
-              Submit
-            </button>
+            <div className="flex justify-between items-center">
+              <div>
+                <span className="text-gray-800">
+                  Total: ${calculateTotalPrice()}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <button
+                  className="w-20 h-10 bg-red-500 text-white rounded-md mr-2"
+                  onClick={clearCart}
+                >
+                  Clear
+                </button>
+                <button
+                  className="w-20 h-10 bg-green-500 text-white rounded-md"
+                  onClick={submitOrder}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-          </div>
+          </>
         ) : (
-          <div className="flex flex-col justify-center items-center text-3xl text-rose-500 py-20 px-4">
-            Your Cart Is Empty
-          </div>
+          <p className="text-center text-gray-800">Your cart is empty</p>
         )}
-      </aside>
-    </>
+      </div>
+    </aside>
   );
 };
 
 Cart.propTypes = {
-  showCart: PropTypes.func,
+  showCart: PropTypes.func.isRequired,
 };
 
 export default Cart;
